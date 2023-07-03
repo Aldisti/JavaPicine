012d7d12e22d3addea13d8c04f363dc13a6a2e66
Aldisti <alessandrodiste99@gmail.com>
2023-07-03 14:31:55 +0200

    ex03 done

--- Java00/ex03/Program.java
+++ Java00/ex03/Program.java
@@ -5,19 +5,25 @@ class Program
 	public static void	main(String[] args)
 	{
 		long	values = 0;
-		long	esp = 1000000000000000000;
-		int		num = 0;
-		long	tmp = 10;
+		long	esp = 1000000000000000000L;
+		int		num;
+		long	tmp;
 		String	week;
 		Scanner	kb = new Scanner(System.in);
+
+		System.out.print("->");
 		week = kb.nextLine();
-		for (int i = 1; i <= 18 && !String::equals(week, "42"); i++)
+		for (int i = 1; i <= 18 && !week.equals("42"); i++)
 		{
-			if (!String::equals(week, "week" + i))
+			num = 0;
+			tmp = 10;
+			if (!week.equals( "Week " + i))
 			{
+				System.out.println(week);
 				System.err.println("IllegalArgument");
 				System.exit(-1);
 			}
+			System.out.print("->");
 			for (int j = 0; j < 5; j++)
 			{
 				num = kb.nextInt();
@@ -26,8 +32,21 @@ class Program
 			}
 			values += tmp * esp;
 			esp /= 10;
+			System.out.print("->");
+			kb.nextLine();
+			week = kb.nextLine();
 		}
-		
+		esp = 1000000000000000000L;
+		for (int i = 1; i <= 18 && values / esp != 0; i++)
+		{
+			System.out.print("Week " + i + " ");
+			tmp = values / esp;
+			values -= tmp * esp;
+			esp /= 10;
+			for (int j = 0; j < tmp; j++)
+				System.out.print("=");
+			System.out.println(">");
+		}
 	}
 }
 
