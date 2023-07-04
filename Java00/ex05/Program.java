a45b77fb0a4674602987ee0e6d63769d0297642e
Aldisti <alessandrodiste99@gmail.com>
2023-07-04 15:04:21 +0200

    ex05 finished

--- Java00/ex05/Program.java
+++ Java00/ex05/Program.java
@@ -7,9 +7,10 @@ class Program {
 	private static final int	STUDENTS = 10;
 	private static final int	MAX_INPUT = 10;
 	private static final int	DAYS_MAP[] = {1, -1, -1, -1, 2, 5, -1, 4, 6, -1, 3, -1, -1, -1, 0};
+	private static final String	WEEK_MAP[] = {"TU", "WE", "TH", "FR", "SA", "SU", "MO"};
 
 	public static int	getDayIndex(char letters[]) {
-		return (DAYS_MAP[((((int)letters[0] * (int)letters[1]) % 1000) / 10) % 16]);
+		return (DAYS_MAP[((((int)letters[1] * (int)letters[2]) % 1000) / 10) % 16]);
 	}
 
 	public static void	addLesson(int days[][], char c, String line) {
@@ -17,11 +18,8 @@ class Program {
 		int	day = getDayIndex(line.toCharArray());
 
 		
-		//System.out.println("day: " + day + " hour: " + hour);
 		for (; day < DAYS; day += 7) {
-			//System.out.println("day: " + day + " hour: " + hour);
 			days[day][hour] = 1;
-			//System.out.println("day: " + days[day][hour]);
 		}
 	}
 
@@ -37,16 +35,59 @@ class Program {
 	public static void	sign(int days[][], int index, Scanner kb) {
 		int		hour = kb.nextInt() - 1;
 		int		day = kb.nextInt() - 1;
-		boolean	presence = kb.nextLine().equals("HERE");
+		boolean	presence = kb.nextLine().equals(" HERE");
 		int		tmp = 2;
 
 		if (presence)
 			tmp = 1;
 		tmp = tmp << (index * 2 + 1);
-		//System.out.println("tmp: " + tmp);
-		//System.out.println("B day: " + days[day][hour]);
 		days[day][hour] |= tmp;
-		//System.out.println("A day: " + days[day][hour]);
+	}
+
+	public static void	printPadded(String str, int pad) {
+		for (int i = 0; i < pad - str.length(); i++) {
+			System.out.print(" ");
+		}
+		System.out.print(str);
+	}
+
+	public static void	print(int days[][], String names[]) {
+		System.out.print("          ");
+		for (int i = 0; i < DAYS; i++) {
+			for (int j = 0; j < LESSONS; j++) {
+				if (days[i][j] % 2 == 1) {
+					System.out.print((j + 1) + ":00 " + WEEK_MAP[i % 7] + " ");
+					if (i > 10) {
+						System.out.print((i + 1) + "|");
+					}
+					else {
+						System.out.print(" " + (i + 1) + "|");
+					}
+				}
+			}
+		}
+		System.out.println();
+		for (int i = 0; i < STUDENTS && names[i] != null; i++) {
+			printPadded(names[i], 10);
+			for (int j = 0; j < DAYS; j++) {
+				for (int k = 0; k < LESSONS; k++) {
+					if (days[j][k] % 2 == 1) {
+						switch (days[j][k] >> (i * 2 + 1) & 3) {
+							case 1:
+								printPadded("1", 10);
+								break ;
+							case 2:
+								printPadded("-1", 10);
+								break ;
+							default:
+								printPadded("", 10);
+						}
+						System.out.print("|");
+					}
+				}
+			}
+			System.out.println();
+		}
 	}
 
 	public static void	main(String[] args) {
@@ -84,14 +125,7 @@ class Program {
 			sign(days, findStudent(names, str), kb);
 		}
 		kb.nextLine();
-
-		for (int i = 0; i < DAYS; i++) {
-			System.out.println("Day: " + (i + 1));
-			for (int j = 0; j < LESSONS; j++) {
-				System.out.print("	" + (j + 1) + ": " + days[i][j]);
-			}
-			System.out.println();
-		}
+		print(days, names);
 	}
 }
 
