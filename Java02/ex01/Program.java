316ee2b8741ca6f57857c8c324de1c7d4fae7425
Aldisti <alessandrodiste99@gmail.com>
2023-07-13 15:33:25 +0200

    ex01 really done

--- Java02/ex01/Program.java
+++ Java02/ex01/Program.java
@@ -3,6 +3,7 @@ class Program {
 
     public static void main(String[] args){
 		if (args.length != 2) {
+			System.out.println("Invalid arguments");
 			return ;
 		}
 		System.out.printf("Similarity = %.3f\n", new Similarity(new File(args[0]), new File(args[1])).similarity());
