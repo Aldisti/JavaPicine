525ee256dfd49641f3e81d0db0d19acbdcc191c8
Aldisti <alessandrodiste99@gmail.com>
2023-07-13 15:00:12 +0200

    ex03 done

--- Java03/ex03/Program.java
+++ Java03/ex03/Program.java
@@ -1,43 +1,5 @@
 import java.util.*;
 import java.io.*;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
-class DownloadThread extends Thread {
-	private Vector<String>	urls;
-	private int				id;
-	private static int		size = 0;
-	private static Object	lock = new Object();
-
-	public	DownloadThread(Vector<String> urls, int index) {
-		this.urls = urls;
-		this.id = index + 1;
-	}
-
-	public void	run() {
-		String	tmp;
-		int		n;
-
-		while (this.urls.size() > 0) {
-			synchronized (lock) {
-				tmp = urls.get(0);
-				urls.remove(0);
-				n = ++size;
-			}
-			System.out.println("Thread-" + this.id + " start download file number " + n);
-			try {
-				InputStream in = new URL(tmp).openStream();
-				Files.copy(in, Paths.get(tmp.substring(tmp.lastIndexOf('/') + 1, tmp.length())));
-			}
-			catch (Exception e) {
-				System.out.println(e.getMessage());
-				return ;
-			}
-			System.out.println("Thread-" + this.id + " finish download file number " + n);
-		}
-	}
-}
 
 class Program {
 
