a42388106b0018947d9c54d62342272abf9f17c1
Aldisti <alessandrodiste99@gmail.com>
2023-07-18 12:43:17 +0200

    ex00 ongoing

---
+++ Java05/ex00/Message.java
@@ -0,0 +1,65 @@
+import java.util.*;
+
+public class Message {
+
+	private int			id;
+	private User		author;
+	private Chatroom	room;
+	private String		text;
+	private String		time;
+
+	public	Message(int id, User author, Chatroom room, String text, String time) {
+		this.id = id;
+		this.author = author;
+		this.room = room;
+		this.text = text;
+		this.time = time;
+	}
+
+	public int			getId() {
+		return (this.id);
+	}
+
+	public User			getAuthor() {
+		return (this.author);
+	}
+
+	public Chatroom		getRoom() {
+		return (this.room);
+	}
+
+	public String		getText() {
+		return (this.text);
+	}
+
+	public String		getTime() {
+		return (this.time);
+	}
+
+	@Override
+	public boolean		equals(Object o) {
+		if (this == o) {
+			return (true);
+		}
+		if (o == null || this.getClass() != o.getClass()) {
+			return (false);
+		}
+		Message	m = (Message) o;
+		return (this.id == m.getId()
+				&& this.author.equals(m.getAuthor())
+				&& this.room.equals(m.getRoom())
+				&& this.text.equals(m.getText())
+				&& this.time.equals(m.getTime()));
+	}
+
+	@Override
+	public int		hashCode() {
+		return (Objects.hash(this.id, this.author, this.room, this.text, this.time));
+	}
+
+	@Override
+	public String	toString() {
+		return ("id: " + this.id + "\tauthor: " + this.author.getLogin()
+				+ "\troom: " + this.room.getName() + "\ttime: " + this.time);
+	}
+}
---
+++ Java05/ex00/data.sql
@@ -0,0 +1,19 @@
+
+INSERT INTO chat.user(login, pass) VALUES ('adi-stef', '123')
+INSERT INTO chat.user(login, pass) VALUES ('gpanico', '456');
+INSERT INTO chat.user(login, pass) VALUES ('mpaterno', '789');
+INSERT INTO chat.user(login, pass) VALUES ('lde-mich', '321');
+INSERT INTO chat.user(login, pass) VALUES ('dcastagn', '654');
+
+INSERT INTO chat.room(name, owner) VALUES ('aaa', (SELECT id FROM chat.user WHERE id = 1);
+INSERT INTO chat.room(name, owner) VALUES ('bbb', (SELECT id FROM chat.user WHERE id = 2);
+INSERT INTO chat.room(name, owner) VALUES ('ccc', (SELECT id FROM chat.user WHERE id = 3);
+INSERT INTO chat.room(name, owner) VALUES ('ddd', (SELECT id FROM chat.user WHERE id = 4);
+INSERT INTO chat.room(name, owner) VALUES ('eee', (SELECT id FROM chat.user WHERE id = 5);
+
+INSERT INTO chat.message(author, room, text, time) VALUES (1, 1, 'ciao', '2023-07-18 12:42:42');
+INSERT INTO chat.message(author, room, text, time) VALUES (2, 2, 'come', '2023-07-18 12:42:42');
+INSERT INTO chat.message(author, room, text, time) VALUES (3, 3, 'stai', '2023-07-18 12:42:42');
+INSERT INTO chat.message(author, room, text, time) VALUES (4, 4, 'sono', '2023-07-18 12:42:42');
+INSERT INTO chat.message(author, room, text, time) VALUES (5, 5, 'nero', '2023-07-18 12:42:42');
+
