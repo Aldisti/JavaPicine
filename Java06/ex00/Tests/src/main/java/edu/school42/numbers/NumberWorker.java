c98169f7aa75c97cb8bffd9d9bc37ef3b0577346
Aldisti <alessandrodiste99@gmail.com>
2023-07-21 16:18:07 +0200

    ex00 done

--- Java06/ex00/Tests/src/main/java/edu/school42/numbers/NumberWorker.java
+++ Java06/ex00/Tests/src/main/java/edu/school42/numbers/NumberWorker.java
@@ -5,26 +5,26 @@ public class NumberWorker {
 
 	public boolean	isPrime(int number) {
 		if (number <= 1) {
-			throw new IllegalNumberException();
+			throw new IllegalNumberException("IllegalNumberException");
 		}
 		if (number % 2 == 0) {
 			return (number == 2);
 		}
 		for (int i = 3; i <= number / i; i += 2) {
 			if (number % i == 0) {
-				return (true);
+				return (false);
 			}
 		}
-		return (false);
+		return (true);
 	}
 
-	public int	digitSum(int number) {
+	public int	digitsSum(int number) {
 		int	out = 0;
 
 		while (number != 0) {
 			out += number % 10;
 			number /= 10;
 		}
-		return (out);
+		return ((out >= 0) ? out : -out);
 	}
 }
