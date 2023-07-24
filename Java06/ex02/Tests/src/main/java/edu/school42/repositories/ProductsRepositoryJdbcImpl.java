b3b3b8124a3222e713542a7003746301f0f48c4c
Aldisti <alessandrodiste99@gmail.com>
2023-07-24 15:58:59 +0200

    testing

--- Java06/ex02/Tests/src/main/java/edu/school42/repositories/ProductsRepositoryJdbcImpl.java
+++ Java06/ex02/Tests/src/main/java/edu/school42/repositories/ProductsRepositoryJdbcImpl.java
@@ -69,7 +69,6 @@ public class ProductsRepositoryJdbcImpl implements ProductsRepository {
 		}
 		query += ", price = " + product.getPrice();
 		query += " WHERE id = " + product.getId();
-		System.out.println("update: " + query);
 		try {
 			this.con.prepareStatement(query).execute();
 		}
@@ -92,6 +91,7 @@ public class ProductsRepositoryJdbcImpl implements ProductsRepository {
 			query += ", " + "'null'";
 		}
 		query += ", " + product.getPrice() + ")";
+		System.out.println("\nsave: " + query + "\n");
 		try {
 			this.con.prepareStatement(query).execute();
 		}
