d36abfcf0afa5c01d28a4f8b3b91ead0790ae6e7
Aldisti <alessandrodiste99@gmail.com>
2023-07-24 16:06:14 +0200

    testing

--- Java06/ex01/Tests/src/test/resources/data.sql
+++ Java06/ex01/Tests/src/test/resources/data.sql
@@ -1,7 +1,7 @@
 
-INSERT INTO product(id, name, price) VALUES (1, 'Galaxy S21 5G1, ', 600);
-INSERT INTO product(id, name, price) VALUES (2, 'Gear S3 Frontier1, ', 300);
-INSERT INTO product(id, name, price) VALUES (3, 'Mi True Earbuds1, ', 25);
-INSERT INTO product(id, name, price) VALUES (4, 'Lenovo Legion 51, ', 1500);
-INSERT INTO product(id, name, price) VALUES (5, 'Gan iCarry 3651, ', 40);
+INSERT INTO products(id, name, price) VALUES (1, 'Galaxy S21 5G1, ', 600);
+INSERT INTO products(id, name, price) VALUES (2, 'Gear S3 Frontier1, ', 300);
+INSERT INTO products(id, name, price) VALUES (3, 'Mi True Earbuds1, ', 25);
+INSERT INTO products(id, name, price) VALUES (4, 'Lenovo Legion 51, ', 1500);
+INSERT INTO products(id, name, price) VALUES (5, 'Gan iCarry 3651, ', 40);
 
